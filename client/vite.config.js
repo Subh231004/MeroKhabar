633eede00210e52d8b22b55a44db3bbcafb11505
ABHISHEK <harshroy0501@gmail.com>
2025-02-24 21:50:55 +0530

    v1.2

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    sourcemap: false,
-    emptyOutDir: true
+    outDir: path.resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        dir: path.resolve(__dirname, 'dist')
+      }
+    }
   },
   server: {
     port: 3000
