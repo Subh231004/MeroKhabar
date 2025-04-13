9ac9d32b4f39f571edd694e4dcbceeec7a5ebdb3
Subh231004 <153432046+Subh231004@users.noreply.github.com>
2025-04-13 22:50:22 +0530

    "firebase connection"

--- models/ArticleSchema.js
+++ models/ArticleSchema.js
@@ -1,22 +1,22 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const ArticleSchema = new mongoose.Schema({
-//     title: {
-//         type: String,
-//         required: true,
-//     },
-//     content:{
-//         type: String,
-//         required: true,
-//     },
-//     category: {
-//         type: String,
-//         required: true,      
-//     },
-//     date: {
-//         type: Date,
-//         default: Date.now,
-//     },
-// });
+const ArticleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content:{
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,      
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
 
-// module.exports = mongoose.model('Article',ArticleSchema);
+module.exports = mongoose.model('Article',ArticleSchema);
--- server/server.js
+++ server/server.js
@@ -1,37 +1,37 @@
-  const express = require("express");
-  const cors = require("cors");
-  const mongoose = require("mongoose");
+//  const express = require("express");
+//  const cors = require("cors");
+//  const mongoose = require("mongoose");
 
-  const apiRoutes = require("./routes/api");
+//  const apiRoutes = require("./routes/api");
 
-  const app = express();
+//  const app = express();
 
-  mongoose
-    .connect("mongodb+srv://user:user%40123@merokhabhar.vzedb.mongodb.net/MK", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => console.log("Connected to MongoDB Atlas"))
-    .catch((err) => console.log(err));
+//  mongoose
+//    .connect("mongodb+srv://user:user%40123@merokhabhar.vzedb.mongodb.net/MK", {
+//      useNewUrlParser: true,
+//      useUnifiedTopology: true,
+//    })
+//    .then(() => console.log("Connected to MongoDB Atlas"))
+//    .catch((err) => console.log(err));
 
-  app.use(cors());
-  app.use(express.json());
+//  app.use(cors());
+//  app.use(express.json());
 
-  app.use("/api", apiRoutes);
+//  app.use("/api", apiRoutes);
 
-  const PORT = 3001;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//  const PORT = 3001;
+//  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-// const express = require("express");
-// const connectDB = require('./db');
-// const app = express();
+const express = require("express");
+const connectDB = require('./db');
+const app = express();
 
-// connectDB();
+connectDB();
 
-// app.use(express.json());
+app.use(express.json());
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-// app.listen(PORT, () => console.log('Server running on port ${PORT}'));
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
 
-// require('dotenv').config();
+require('dotenv').config();
